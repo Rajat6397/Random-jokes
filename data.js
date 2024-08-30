1645a7221cdada16db436f86db3778650036d9a8
Rajat <rajatsaxena6397@gmail.com>
2024-08-30 23:20:35 +0530

    commit

---
+++ data.js
@@ -0,0 +1,27 @@
+const jokes = [
+    {
+        id:0,
+        joke:"Why did the math book look sad? Because it had too many problems."
+    },
+    {
+        id: 1,
+        joke:"Why don’t scientists trust atoms? Because they make up everything!"
+    },
+    {
+        id:2,
+        joke:"Why did the scarecrow win an award? Because he was outstanding in his field!"
+    },
+    {
+        id:3,
+        joke:"Why don’t skeletons fight each other? They don’t have the guts."
+    },
+    {
+        id:4,
+        joke:"Why can't you give Elsa a balloon? Because she will let it go!"
+    },
+    {
+        id:5,
+        joke:"I used to play piano by ear, but now I use my hands."
+    }
+];
+module.exports = jokes;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const jokes = require('./data');
+
+const app = express();
+
+const PORT = 8080;
+
+app.get('/jokes', (req, res)=>{
+  const requiredRandomJokeIndex = Math.floor(Math.random() * jokes.length);
+  return res.status(200).json(jokes[requiredRandomJokeIndex]);
+})
+app.get('/joke/:jokeId', (req, res)=>{
+    const jokeId = req.params.jokeId;
+    // console.log(jokeId);
+    
+    if(!Number(jokeId) || Number(jokeId)<0){
+        return res.status(400).json({error:"Invalid joke Id"});
+    }
+    return res.status(200).json(jokes.find(joke=>joke.id==jokeId));
+});
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+    
+});
